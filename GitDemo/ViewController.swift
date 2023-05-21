3c1482bc329f7a90d5c1530f81481772e6f7d5ce
arbazkhan9393 <arbazkhan9393@gmail.com>
2023-05-21 14:15:32 +0500

    subtract two numbers

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var a = 20
+    var b = 10
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        subtractTwoNumbers()
     }
 
 
+    func subtractTwoNumbers() {
+        NSLog("Subtraction of %i and %i is %i", a , b, (a-b))
+    }
 }
 
